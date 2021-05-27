f5404f24ddbdee545056dd72f84c0b132c8cd078
Himanshu Amreliya <https://github.com/HimanshuAmreliya/>
2021-05-27 19:30:58 +0530

    DOTNET

---
+++ StudentManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number Of Students");
+            var studentCount = int.Parse(Console.ReadLine());
+
+            var studentNames = new String[studentCount];
+            var studentGrades = new int [studentCount];
+
+            for (int i = 0; i < studentCount; i++)
+            {
+                Console.WriteLine("Enter Name of Student :");
+                studentNames[i] = Console.ReadLine();
+                Console.WriteLine("Enter Student Grade :");
+                studentGrades[i] = int.Parse(Console.ReadLine());
+
+
+            }
+                for (int i = 0; i < studentCount; i++)
+                {
+                    Console.WriteLine("Student Name - {0} , Grade - {1}  ", studentNames[i],  studentGrades[i]);
+                }
+            
+            }
+    }
+}
---
+++ Survey.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Survey1
+{
+    class Data {
+        public String name;
+        public int age;
+        public String month;
+
+        public void Info() {
+            Console.WriteLine("your name is - {0}", name);
+            Console.WriteLine("your age is - {0}", age);
+            Console.WriteLine("your birth month is - {0}", month);
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new Data();
+
+            Console.WriteLine("what is your name ?");
+            data.name = Console.ReadLine();
+            Console.WriteLine("what is your age ?");
+            data.age = int.Parse(Console.ReadLine());
+            Console.WriteLine("what is your birth month ?");
+            data.month = Console.ReadLine();
+
+            data.Info();
+        }
+    }
+}
